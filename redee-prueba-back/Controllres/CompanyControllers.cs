bc55614bb60996627e382d98a18cd60ab3dd4627
Neryad <dayern.gomez@hotmail.com>
2025-04-26 16:06:14 -0400

    Add CompanyControllers and update database migrations
    Introduces a new `CompanyControllers` class for handling HTTP requests related to companies, leveraging Entity Framework Core for database operations. Implements methods for retrieving, updating, and creating company records. Updates migration files to define the `Companies` table structure and modifies `ApplicationDbContext` to include the new `Company` entity.

--- redee-prueba-back/Controllres/CompanyControllers.cs
+++ redee-prueba-back/Controllres/CompanyControllers.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using redee_prueba_back.Data;
+using redee_prueba_back.Entities;
 
 namespace redee_prueba_back.Controllres
 {
@@ -6,10 +9,54 @@ namespace redee_prueba_back.Controllres
     [Route("api/companies")]
      public class CompanyControllers : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public CompanyControllers(ApplicationDbContext applicationDbContext)
         {
-            return "Hello from CompanyControllers";
+            this.applicationDbContext = applicationDbContext;
+        }
+
+      
+        [HttpGet]
+        public async Task<IEnumerable<Company>> Get ()
+        {
+            return await applicationDbContext.Companies.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Company>> Get(int id)
+        {
+            var company = await applicationDbContext.Companies.FirstOrDefaultAsync(x => x.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return company;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, Company company)
+        {
+            if (id != company.Id)
+            {
+                return BadRequest("Id no coicinde");
+            }
+            applicationDbContext.Update(company);
+            await applicationDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Company company)
+        {
+            if (company == null)
+            {
+                return BadRequest("Company cannot be null");
+            }
+            applicationDbContext.Companies.Add(company);
+            await applicationDbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = company.Id }, company);
+
         }
 
     }
---
+++ redee-prueba-back/Migrations/20250426193951_CompanyTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace redee_prueba_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rnc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommercialName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Payment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Activity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
