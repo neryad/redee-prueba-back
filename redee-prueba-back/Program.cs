35b8c06e209a7bb06ca69a76600622eedf8d38a8
Neryad <dayern.gomez@hotmail.com>
2025-04-26 15:38:01 -0400

    Update connection string configuration in Program.cs
    Modified the `ApplicationDbContext` setup to retrieve the connection string from the application's configuration using `"name=DefaultConnection"` instead of hardcoding it. This change improves flexibility by allowing connection strings to be managed through configuration files or environment variables.

--- redee-prueba-back/Program.cs
+++ redee-prueba-back/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("DefaultConnection"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
